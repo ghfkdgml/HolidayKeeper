e270d9fbf50bc899477d34d6545dbd190ead9a87
sh <suho1898@naver.com>
2025-12-02 09:27:06 +0900

    feat(api): api 관련 config 파일 정리

--- holidayKeeper/src/main/java/com/ksh/holidayKeeper/config/RestClientConfig.java
+++ holidayKeeper/src/main/java/com/ksh/holidayKeeper/config/RestClientConfig.java
@@ -1,4 +1,4 @@
-package com.ksh.holidayKeeper.api;
+package com.ksh.holidayKeeper.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
